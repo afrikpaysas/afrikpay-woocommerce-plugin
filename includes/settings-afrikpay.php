d34efb3697751e506e3b0226dc68d86775f57427
tresor.simo <tresor.simo@afrikpay.com>
2019-12-13 18:50:51 +0100

    push

---
+++ includes/settings-afrikpay.php
@@ -0,0 +1,43 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Settings for PayPal Gateway.
+ */
+return array(
+	'enabled' => array(
+		'title'   => __( 'Enable/Disable', 'woocommerce' ),
+		'type'    => 'checkbox',
+		'label'   => __( 'Enable Afrikpay', 'woocommerce' ),
+		'default' => 'no',
+	),
+	'title' => array(
+		'title'       => __( 'Title', 'woocommerce' ),
+		'type'        => 'text',
+		'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
+		'default'     => __( 'Afrikpay', 'woocommerce' ),
+		'desc_tip'    => true,
+	),
+	'description' => array(
+		'title'       => __( 'Description', 'woocommerce' ),
+		'type'        => 'text',
+		'desc_tip'    => true,
+		'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce' ),
+		'default'     => __( 'Payer avec Afrikpay;', 'woocommerce' ),
+	),
+	'merchantid' => array(
+		'title'       => __( 'Afrikpay Merchant ID', 'woocommerce' ),
+		'type'        => 'text',
+		'desc_tip'    => true,
+		'description' => __( 'Please enter your Afrikpay Merchant ID; this is needed in order to take payment.', 'woocommerce' ),
+	),
+	'urlafrikpay' => array(
+		'title'       => __( 'URL Afrikpay', 'woocommerce' ),
+		'type'        => 'text',
+		'desc_tip'    => true,		
+		'description' => __( 'URL Afrikpay', 'woocommerce' ),
+	),
+);
